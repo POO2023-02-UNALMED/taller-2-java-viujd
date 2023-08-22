283cd7c83b6944ca388d760ee286205b6d3d4aa2
Juan David Robayo <141962352+viujd@users.noreply.github.com>
2023-08-21 23:31:24 -0500

    Agregar metodos y atributos a la clase

--- src/test/java/test/Asiento.java
+++ src/test/java/test/Asiento.java
@@ -1,5 +1,15 @@
 package test;
 
 public class Asiento {
+	String color;
+	int precio;
+	int registro;
+	
+	void cambiarColor(String color) {
+		if (color=="rojo" || color=="verde" || color=="amarillo" || color=="negro" || color=="blanco")
+		{
+			this.color=color;
+		}
+	}
 
 }
